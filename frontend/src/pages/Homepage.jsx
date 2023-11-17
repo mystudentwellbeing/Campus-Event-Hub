6260464364329f0700cc1daee2a5c96e41ece7a2
topiwalaj <topiwalj@outlook.com>
2023-11-17 16:58:02 -0500

    added json data for events and work on homepage component

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,48 @@
+import styles from './Homepage.module.css';
+import Event from './Event';
+import eventsData from '../data/events.json';
+
+// Access the 'events' array within the 'events' object
+const events = eventsData.events
+// Check the structure of 'events' here
+console.log(events); 
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  //return <div>HomePage</div>;
+  return (
+    <>
+    <div className={styles.serchbar}>
+      <div>
+        <p className={styles.serchbox}>Search Events!</p>
+      </div>
+      <div className={styles.filtericons}>
+        <button>NETWORKING</button>
+        <button>CAMPUS</button>
+        <button>CULTURAL</button>            
+        <button>HOBBIES</button>
+        <button>SPORTS</button>
+        <button>EDUCATIONAL</button>                
+        <button>NIGHTLIFE</button>
+        <button>ARTS</button>                
+        <button>WELLBEING</button>
+      </div>
+    </div>
+    <div className={styles.allevents}>
+      {events.map((event,index) => (
+        <Event 
+          key={index}
+          image_url={event.image_url} 
+          name_of_event={event.name_of_event} 
+          price={event.price} 
+          date={event.date} 
+          time={event.time} 
+          type={event.type} 
+          short_description={event.short_description}
+        />
+      ))}
+    </div>
+    </>
+  )
 };
 
 export default HomePage;
