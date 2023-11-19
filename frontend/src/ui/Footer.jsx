ebbd0c54d0e98c6ff5e5b76e8b6526f30f88fa94
Kelly Lee <kellyws.lee@gmail.com>
2023-11-19 10:32:02 -0500

    Add footer and media query for footer

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -1,3 +1,4 @@
+import { MdOutlineEmail } from 'react-icons/md';
 import { SlSocialInstagram } from 'react-icons/sl';
 import { SlSocialFacebook } from 'react-icons/sl';
 import { SlSocialLinkedin } from 'react-icons/sl';
@@ -6,12 +7,20 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.email}>Email</div>
-      <div className={styles.terms}></div>
-      <div className={styles.social}>
-        <SlSocialInstagram className={styles.reactIcons} />
-        <SlSocialFacebook className={styles.reactIcons} />
-        <SlSocialLinkedin className={styles.reactIcons} />
+      <div className={styles.footerContainer}>
+        <div className={styles.email}>
+          <MdOutlineEmail className={styles.reactIcons} />
+          <p>admin@campuseventhub.ca</p>
+        </div>
+        <div className={styles.terms}>
+          <p>Terms of Use and Privacy Policy</p>
+          <p>&copy; All rights reserved</p>
+        </div>
+        <div className={styles.social}>
+          <SlSocialInstagram className={styles.reactIcons} />
+          <SlSocialFacebook className={styles.reactIcons} />
+          <SlSocialLinkedin className={styles.reactIcons} />
+        </div>
       </div>
     </footer>
   );
