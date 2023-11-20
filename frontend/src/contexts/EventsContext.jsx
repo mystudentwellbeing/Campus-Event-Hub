51e7df7d72334631491a3d274f2eaff4365f93bd
Kelly Lee <kellyws.lee@gmail.com>
2023-11-19 21:30:26 -0500

    Add filter functions

--- frontend/src/contexts/EventsContext.jsx
+++ frontend/src/contexts/EventsContext.jsx
@@ -7,22 +7,31 @@ const EventsProvider = ({ children }) => {
   const events = eventsData.events;
 
   const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState('');
   // const [isLoading, setLoading] = useState(true);
   // const [error, setError] = useState('');
 
-  const filteredEvents = (
-    query
-      ? events.filter(
-          (event) =>
-            event.name_of_event.toLowerCase().includes(query.toLowerCase()) ||
-            event.description.toLowerCase().includes(query.toLowerCase()) ||
-            event.type.toLowerCase().includes(query.toLowerCase())
-        )
-      : events
-  ).sort((a, b) => a.date.localeCompare(b.date));
+  const filteredEvents = events
+    .filter((event) => {
+      const matchesQuery =
+        !query ||
+        event.name_of_event.toLowerCase().includes(query.toLowerCase()) ||
+        event.description.toLowerCase().includes(query.toLowerCase()) ||
+        event.type.toLowerCase().includes(query.toLowerCase());
+      const eventTypes = event.type
+        .split(',')
+        .map((type) => type.trim().toUpperCase());
+      const matchesFilter = filter
+        ? eventTypes.includes(filter.toUpperCase())
+        : true;
+      return matchesQuery && matchesFilter;
+    })
+    .sort((a, b) => a.date.localeCompare(b.date));
 
   return (
-    <EventsContext.Provider value={{ filteredEvents, query, setQuery }}>
+    <EventsContext.Provider
+      value={{ filteredEvents, query, setQuery, setFilter }}
+    >
       {children}
     </EventsContext.Provider>
   );
--- frontend/src/features/Filter.jsx
+++ frontend/src/features/Filter.jsx
@@ -1,3 +1,4 @@
+import useEvents from '../hooks/useEvents';
 import Button from '../ui/Button';
 import { GrGroup } from 'react-icons/gr';
 import { LuSchool } from 'react-icons/lu';
@@ -11,41 +12,47 @@ import { LuFlower2 } from 'react-icons/lu';
 import styles from './Filter.module.css';
 
 const Filter = () => {
+  const { setFilter } = useEvents();
+
+  const handleFilterChange = (filterType) => {
+    setFilter(filterType);
+  };
+
   return (
     <div className={styles.filterContainer}>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('NETWORKING')}>
         <GrGroup className={styles.filterIcon} />
         NETWORKING
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('CAMPUS')}>
         <LuSchool className={styles.filterIcon} />
         CAMPUS
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('CULTURAL')}>
         <IoEarthOutline className={styles.filterIcon} />
         CULTURAL
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('HOBBIES')}>
         <MdOutlineHeadset className={styles.filterIcon} />
         HOBBIES
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('SPORTS')}>
         <MdOutlineSportsBasketball className={styles.filterIcon} />
         SPORTS
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('EDUCATIONAL')}>
         <SlGraduation className={styles.filterIcon} />
         EDUCATIONAL
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('NIGHTLIFE')}>
         <MdNightlife className={styles.filterIcon} />
         NIGHTLIFE
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('ARTS')}>
         <MdOutlineColorLens className={styles.filterIcon} />
         ARTS
       </Button>
-      <Button type="filter">
+      <Button type="filter" onClick={() => handleFilterChange('WELLBEING')}>
         <LuFlower2 className={styles.filterIcon} />
         WELLBEING
       </Button>
