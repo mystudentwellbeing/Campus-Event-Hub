c020b09813c3b55a654f853fbf288cf78bfca70c
Kelly Lee <kellyws.lee@gmail.com>
2023-11-21 19:11:40 -0500

    Set up MySubmitted Event page structure

--- frontend/src/features/MySubmittedEvents.jsx
+++ frontend/src/features/MySubmittedEvents.jsx
@@ -1,13 +1,13 @@
-import styles from './MySavedEvents.module.css';
+import styles from './MySubmittedEvents.module.css';
 const MySubmittedEvents = () => {
   return (
-    <main>
+    <main className={styles.container}>
       <h1>My Submitted Events</h1>
-      <div>
-        <section>
+      <div className={styles.sectionContainer}>
+        <section className={styles.approved}>
           <h2>Approved Events</h2>
         </section>
-        <section>
+        <section className={styles.pending}>
           <h2>Pending Events</h2>
         </section>
       </div>
