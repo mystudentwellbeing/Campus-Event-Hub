ea6d9704653dade89950a75b46dcdc68d9ab6bae
samihasher <you@example.com>
2023-11-23 17:34:31 -0500

    updated css and formating on event pages

--- frontend/src/features/EventFullInfo.jsx
+++ frontend/src/features/EventFullInfo.jsx
@@ -3,6 +3,22 @@ import { useEffect } from 'react';
 import useEvents from '../hooks/useEvents';
 import styles from './EventFullInfo.module.css';
 
+const formatEventDate = (date) => {
+  const dateOptions = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+  const [year, month, day] = date.split('-');
+  const formattedDate = new Date(year, month - 1, day).toLocaleDateString(
+    'en-US',
+    dateOptions
+  );
+
+  return formattedDate;
+};
+
 const EventFullInfo = () => {
   const { id } = useParams();
   const { currentEvent, getEventById } = useEvents();
@@ -14,6 +30,9 @@ const EventFullInfo = () => {
   if (!currentEvent) {
     return <h2>Loading...</h2>;
   }
+
+  const formattedDate = formatEventDate(currentEvent.date);
+
   return (
     <main className={styles.container}>
       <div className={styles.event}>
@@ -25,8 +44,9 @@ const EventFullInfo = () => {
       </div>
 
       <h2 className={styles.name_of_event}>{currentEvent.name_of_event}</h2>
-	<p>{currentEvent.short_description}</p>
-      <p className={styles.dateAndTime}>Date and Time: {currentEvent.date} | Price: {currentEvent.price}</p>
+      <p>{currentEvent.short_description}</p>
+      <p className={styles.dateAndTime}>
+        Date and Time: {formattedDate} | Price: {currentEvent.price}</p>
       <p className={styles.location}>
         Location:<br />
         <span>{currentEvent.name_of_venue}</span><br />
@@ -38,9 +58,16 @@ const EventFullInfo = () => {
         Brought You By<br />
         <span>{currentEvent.name_organization}</span>
       </p>
+
       <p className={styles.descriptionTitle}>Event Description</p>
-      <p>{currentEvent.description}</p>
-      {currentEvent.event_link && <p className={styles.link}>Link: {currentEvent.event_link}</p>}
+      <div className={styles.eventDescription}>{currentEvent.description}</div>
+
+      {currentEvent.event_link && (
+      <p className={styles.link}>
+        <a href={currentEvent.event_link} target="_blank" rel="noopener noreferrer">Link to More Info</a>
+      </p>
+        )
+      }
     </main>
   );
 };
