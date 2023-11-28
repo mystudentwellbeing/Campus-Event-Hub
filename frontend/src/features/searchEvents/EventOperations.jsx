b09d87e22f855c2195f3d184c50a28a6f4b511d9
Kelly Lee <kellyws.lee@gmail.com>
2023-11-28 14:55:41 -0500

    Adjust css on homepage

--- frontend/src/features/searchEvents/EventOperations.jsx
+++ frontend/src/features/searchEvents/EventOperations.jsx
@@ -1,9 +1,22 @@
-import Search from './Search';
+import Search from '../../ui/Search';
+import SortBy from '../../ui/SortBy';
 import Filter from './Filter';
+import styles from './EventOperations.module.css';
+
 const EventOperations = () => {
   return (
-    <section>
-      <Search />
+    <section className={styles.operationContainer}>
+      <div className={styles.searchAndSortWrapper}>
+        <Search />
+        <SortBy
+          options={[
+            { value: 'date-asc', label: 'Sort by date (Old to New)' },
+            { value: 'date-desc', label: 'Sort by date (New to Old)' },
+            { value: 'price-asc', label: 'Sort by price (Low to High)' },
+            { value: 'price-desc', label: 'Sort by price (High to Low)' },
+          ]}
+        />
+      </div>
       <Filter />
     </section>
   );
