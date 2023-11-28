00fd3235824a0fcdef75b36c9202fc911380050e
topiwalaj <topiwalj@outlook.com>
2023-11-28 14:53:01 -0500

    added links for footer

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -5,21 +5,33 @@ import { SlSocialLinkedin } from 'react-icons/sl';
 import styles from './Footer.module.css';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer>
       <div className={styles.footerContainer}>
+      <a href="mailto:topiwalj@outlook.com">
         <div className={styles.email}>
           <MdOutlineEmail className={styles.reactIcons} />
           <p>admin@campuseventhub.ca</p>
         </div>
+      </a>
         <div className={styles.terms}>
           <p>Terms of Use and Privacy Policy</p>
-          <p>&copy; All rights reserved</p>
+          <p>&copy;{year} All rights reserved</p>
         </div>
         <div className={styles.social}>
-          <SlSocialInstagram className={styles.reactIcons} />
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <SlSocialInstagram className={styles.reactIcons} />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <SlSocialFacebook className={styles.reactIcons} />
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <SlSocialLinkedin className={styles.reactIcons} />
+          </a>
+          {/* <SlSocialInstagram className={styles.reactIcons} />
           <SlSocialFacebook className={styles.reactIcons} />
-          <SlSocialLinkedin className={styles.reactIcons} />
+          <SlSocialLinkedin className={styles.reactIcons} /> */}
         </div>
       </div>
     </footer>
