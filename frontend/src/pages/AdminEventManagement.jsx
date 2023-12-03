461187f3395d5fdd301af758f9e2214a1ca7b5aa
Kelly Lee <kellyws.lee@gmail.com>
2023-12-03 17:10:31 -0500

    Update css of EventTable

--- frontend/src/pages/AdminEventManagement.jsx
+++ frontend/src/pages/AdminEventManagement.jsx
@@ -1,14 +1,11 @@
 import EventTableOperations from '../features/admin/EventTableOperations';
 import EventTable from '../features/admin/EventTable';
-import styles from './AdminEventManagement.module.css';
 
 const AdminEventManagement = () => {
   return (
     <main>
-      <div className={styles.mainContainer}>
-        <EventTableOperations />
-        <EventTable />
-      </div>
+      <EventTableOperations />
+      <EventTable />
     </main>
   );
 };
