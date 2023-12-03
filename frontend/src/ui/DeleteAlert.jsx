ff7ed1491adb171919ad5447d8173ab453c06556
Kelly Lee <kellyws.lee@gmail.com>
2023-12-03 17:56:28 -0500

    Update delte modal in the event table

--- frontend/src/ui/DeleteAlert.jsx
+++ frontend/src/ui/DeleteAlert.jsx
@@ -1,22 +1,28 @@
 import { useDeleteEvent } from '../features/submitEvents/useDeleteEvent';
 import Button from './Button';
+import styles from './DeleteAlert.module.css';
 
 const DeleteAlert = ({ eventId, onCloseModal }) => {
   const { deleteEvent } = useDeleteEvent();
   const handleDelete = async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    await deleteEvent(eventId);
+    deleteEvent(eventId);
+    onCloseModal();
+  };
+  const handleCancel = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     onCloseModal();
   };
   return (
-    <>
+    <div className={styles.deleteAlert}>
       <h3>Are you sure you want to delete?</h3>
-      <div>
+      <div className={styles.buttonWrapper}>
         <Button onClick={handleDelete}>Delete</Button>
-        <Button>Cancel</Button>
+        <Button onClick={handleCancel}>Cancel</Button>
       </div>
-    </>
+    </div>
   );
 };
 
