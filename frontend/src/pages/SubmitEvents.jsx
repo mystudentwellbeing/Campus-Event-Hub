a4447b970c361295379666af7265ffa88f10aa2d
Kelly Lee <kellyws.lee@gmail.com>
2024-01-07 16:39:15 -0500

    feat: Add a paragraph under Submit your event

--- frontend/src/pages/SubmitEvents.jsx
+++ frontend/src/pages/SubmitEvents.jsx
@@ -4,6 +4,14 @@ const SubmitEvents = () => {
   return (
     <main>
       <h1>Submit your event</h1>
+      <p style={{ fontSize: '1.2rem' }}>
+        Your contact name, phone number, and email will only be requested by My
+        Student Wellbeing in case additional details regarding your event are
+        needed.
+      </p>
+      <p style={{ fontSize: '1.2rem' }}>
+        This information will be not displayed on the website.
+      </p>
       <SubmitEventForm />
     </main>
   );
